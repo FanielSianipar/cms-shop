416749b4a0047bf30a5b38d8fb48842d8ad66e1e
FanielSianipar <fanielsianipar01@gmail.com>
2023-04-06 00:37:35 +0700

    test

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,7 +12,7 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     You're logged in!
-                    <h1>Welcome {{ Auth::user()->name }}</h1>
+                    <p>Welcome <b>{{ Auth::user()->name }}</b></p>
                 </div>
             </div>
         </div>

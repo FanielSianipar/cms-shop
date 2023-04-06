87daafc213a1782ad0eba5c398a7fc4b16845a00
FanielSianipar <fanielsianipar01@gmail.com>
2023-04-06 16:33:53 +0700

    test

--- resources/views/category-page/category.blade.php
+++ resources/views/category-page/category.blade.php
@@ -11,8 +11,6 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
-                    <br>
                     <a href="{{ url('category/add') }}">
                         <button class="btn btn-primary mb-4">+ Add Category</button>
                     </a>
